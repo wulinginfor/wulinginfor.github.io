7b9a3f8d663db4bf266bc1fa4d225143b36be289
jayin92 <jayin920805@gmail.com>
2020-08-08 10:42:16 +0800

    Site updated: Sat Aug  8 10:42:15 CST 2020

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 generate-md --layout mixu-bootstrap-2col --input src --output .
+# generate-md --layout jasonm23-swiss --input src --output .
 
--- src/index.md
+++ src/index.md
@@ -1,7 +1,8 @@
 # 武陵資訊讀書會
 ## 課程
 ### 8/13
-- PPT：
+- PPT：[jayinnn's blog](https://jayin92.github.io/)
+- 習題：
 
 ### 8/20
 
